1fcdf957b0d24ef6c692d3cba75894ee66af3787
dem.tran.van <dem.tran.van@nextop.asia>
2022-05-01 17:03:26 +0700

    done question 5

--- src/main/java/cracking/array/Question5.java
+++ src/main/java/cracking/array/Question5.java
@@ -9,11 +9,32 @@ public class Question5 {
 
         if (sub > 1) return false;
 
-        if (length1> length2){
-            foundDiff(st2,st1);
+        if (length1 == length2) return foundDiff(st1, st2);
+        if (length1 > length2){
+            return checkDiff(st1,st2);
         }
+        return checkDiff(st2,st1);
+    }
 
-        return foundDiff(st1, st2);
+    public static boolean checkDiff(String st1, String st2) {
+        int start1 = 0;
+        int start2 =0;
+        boolean foundDiff =false;
+
+        while (start1 < st1.length() && start2 < st2.length()){
+            if (st1.charAt(start1) != st2.charAt(start2)){
+                if (foundDiff){
+                    return false;
+                }else {
+                    foundDiff =true;
+                }
+                start1++;
+            }else{
+                start1++;
+                start2++;
+            }
+        }
+        return true;
     }
 
     public static boolean foundDiff(String st1, String st2) {
@@ -30,17 +51,14 @@ public class Question5 {
             }
             start++;
         }
-        if (st1.length() < st2.length() && foundDiff) {
-            return false;
-        }
-        return true;
+        return st1.length() >= st2.length() || !foundDiff;
     }
 
     public static void main(String[] args) {
-        System.out.println(oneway("pale","ple"));
-        System.out.println(oneway("pales","pale"));
-        System.out.println(oneway("pale","bale"));
-        System.out.println(oneway("pale","bake"));
+        System.out.println(oneway("pale", "ple"));
+        System.out.println(oneway("pales", "pale"));
+        System.out.println(oneway("pale", "bale"));
+        System.out.println(oneway("pale", "bake"));
     }
 
 
